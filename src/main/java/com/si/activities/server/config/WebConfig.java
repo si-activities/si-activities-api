5d870c97233d0eca8a1a63d3c51e775bb21e32b4
Arthur <arthurfelipe5567@gmail.com>
2025-11-07 11:34:03 -0300

    fix: add better cors configuration

--- src/main/java/com/si/activities/server/config/WebConfig.java
+++ src/main/java/com/si/activities/server/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.si.activities.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,13 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+  @Value("${cors.allowed.origins}")
+  private String allowedOrigins;
+
   @SuppressWarnings("null")
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedOrigins(allowedOrigins.split(","))
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
-        .allowCredentials(false);
+        .allowCredentials(true);
   }
 }
